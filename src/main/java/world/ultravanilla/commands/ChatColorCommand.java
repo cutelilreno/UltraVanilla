24fb7bbf375da9b4f194b9fd72c8b52fd12efa55
Jamie <160870823+cutelilreno@users.noreply.github.com>
2026-06-13 21:36:11 +0100

    Update ChatColorCommand.java

--- src/main/java/world/ultravanilla/commands/ChatColorCommand.java
+++ src/main/java/world/ultravanilla/commands/ChatColorCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
